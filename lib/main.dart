41e9dbe5cd90ad42c65317117b6ea296af541cc3
Schumi <mattis.litot@epitech.eu>
2024-03-21 17:12:23 +0100

    Update dependencies and add .env file

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ import 'package:social/pages/profile_page.dart';
 import 'package:social/pages/users_pages.dart';
 import 'package:social/theme/dark_mode.dart';
 import 'package:social/theme/light_mode.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 //display error to user
 void displayMessageToUser(String message, BuildContext context) {
@@ -7,4 +8,8 @@ void displayMessageToUser(String message, BuildContext context) {
       builder: (context) => AlertDialog(
             content: Text(message),
           ));
+}
+
+String? getApiKeyFromEnv(String key) {
+  return dotenv.env[key] as String?;
 }
